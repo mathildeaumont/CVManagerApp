d35c0d772fc216687541fcfdafe12fb33093c5fa
mathildeaumont <mathilde_aumont@hotmail.fr>
2014-04-25 19:43:28 +0200

    Home page avec 2 boutons, un pour voir les cv, un pour ajouter un cv

---
+++ src/com/android/CVManagerApp/CVListActivity.java
@@ -0,0 +1,16 @@
+package com.android.CVManagerApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Mathilde on 22/04/14.
+ */
+public class CVListActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list);
+    }
+}
--- src/com/android/CVManagerApp/MyActivity.java
+++ src/com/android/CVManagerApp/MyActivity.java
@@ -1,15 +1,28 @@
 package com.android.CVManagerApp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.main);
+
+        final Button loginButton = (Button) findViewById(R.id.seeCV);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyActivity.this, CVListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
